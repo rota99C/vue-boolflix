c301ab28d111cfb9dfac42d821fdcb2c232170e6
rota99C <rotaboolean@gmail.com>
2021-11-18 15:35:05 +0100

    creata chiamata api dove la query si modifica a seconda dell'input digitato dall'utente

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <input type="text" placeholder="Digita il nome di un film" />
+    <input
+      v-model="queryUtente"
+      type="text"
+      placeholder="Digita il nome di un film"
+    />
     <button @click="submitSearch">cerca</button>
   </div>
 </template>
@@ -10,11 +14,16 @@ import axios from "axios";
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      queryUtente: "",
+    };
+  },
   methods: {
     submitSearch() {
       axios
         .get(
-          "https://api.themoviedb.org/3/search/movie?api_key=7f9dcddd316d242d826f1a99e0924dcb&language=it%3DIT&query=venom&page=1&include_adult=false"
+          `https://api.themoviedb.org/3/search/movie?api_key=7f9dcddd316d242d826f1a99e0924dcb&language=it%3DIT&query=${this.queryUtente}&page=1&include_adult=false`
         )
         .then(function (response) {
           console.log(response.data);
@@ -28,13 +37,4 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  /*   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
-}
 </style>
